a30985ddd900b6f423f2db0ae6275ee8bfe664d4
sasidhar444 <36020918+sasidhar444@users.noreply.github.com>
2019-11-19 10:21:54 +0000

    Update ss.js

--- sample/ss.js
+++ sample/ss.js
@@ -1,11 +1,21 @@
-
 var cost;
-fetch('http://localhost:3000/gcp_price_list')
+/*
+//fetch('http://localhost:3000/gcp_price_list')
+fetch('https://storage.googleapis.com/hostapi/pricelist.json')
 .then((res) => {
     res.json().then((data) => {
          //document.getElementById('paraId').innerHTML = data.skus[0].pricingInfo[0].pricingExpression.tieredRates[0].unitPrice.nanos;
         //console.log(data.skus[0].pricingInfo[0].pricingExpression.tieredRates[0].unitPrice.nanos);
-   console.log(data.STANDARD1.europe_west1);
-     //  console.log(data.sustained_use_tiers_new);
+        document.getElementById('paraId').innerHTML =data.STANDARD1.europe_west1;
+   //console.log(data.STANDARD1.europe_west1);
+       //console.log(data);
+    })
+})*/
+
+
+fetch('https://storage.googleapis.com/hostapi/pricelist.json').then((res)=> {
+    res.json().then((data) => {
+        console.log(data.gcp_price_list.STANDARD1.us_central1);
+        document.getElementById('paraId').innerHTML = data.gcp_price_list.STANDARD1.us_central1;
     })
 })
